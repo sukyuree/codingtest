138db6f8be3f22573fb4448cc61895cb69b53bb6
TaeSeok <sukyuree@naver.com>
2022-07-26 22:36:45 +0900

    Greedy

---
+++ test/src/GreedySugar.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class GreedySugar {
+    public static void main(String[] args){
+    	Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.close();
+        int rtn = 0;
+        loop1:
+        for(int i=0; i<=(N/3); i++){
+            loop2:
+            for(int j=0; j<=(N/5); j++){
+                if(((i*3)+(j*5))==N){
+                    rtn = i+j;
+                    break loop1;
+                }
+                else{
+                    rtn = -1;
+                }
+            }
+        }
+        System.out.println(rtn);
+    }
+}
