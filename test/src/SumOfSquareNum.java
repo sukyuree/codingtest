6879e7e5bba0c02d68d6695fc4ca9353cfe1ad3f
TaeSeok <sukyuree@naver.com>
2022-07-28 21:42:17 +0900

    20220728 CLEAR

---
+++ test/src/SumOfSquareNum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class SumOfSquareNum{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.close();
+        
+        int[] dp = new int[N+1];
+
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; (j * j) <= i; j++){
+                if(dp[i] > dp[i - j*j] + 1){
+                    dp[i] = dp[i - j*j] + 1;
+                }
+            }
+        }
+        System.out.println(dp[N]);
+    }
+}
