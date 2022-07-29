9b9cf0f77bb364bcf88fad7273253adaf30de1eb
TaeSeok <sukyuree@naver.com>
2022-07-29 12:39:16 +0900

    Fix Major

--- test/src/SumOfSquareNum.java
+++ test/src/SumOfSquareNum.java
@@ -9,6 +9,7 @@ public class SumOfSquareNum{
         int[] dp = new int[N+1];
 
         for(int i = 1; i <= N; i++){
+            dp[i] = i;
             for(int j = 1; (j * j) <= i; j++){
                 if(dp[i] > dp[i - j*j] + 1){
                     dp[i] = dp[i - j*j] + 1;
