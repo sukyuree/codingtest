e6d2cc104b3e7dd6aa51f4f19af92579234380be
TaeSeok <sukyuree@naver.com>
2022-08-06 20:56:32 +0900

    20220806 프로그래머스 소수만들기

---
+++ test/src/MakeDecimal.java
@@ -0,0 +1,30 @@
+public class MakeDecimal{
+    public static void main(String[] args){
+    }
+}
+
+
+class Solution{
+    public int solution(int[] nums) {
+        int answer = 0;
+        int sum = 0;
+        boolean isPrime = true;
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j < nums.length; j++){
+                for(int k = j+1; k < nums.length; k++){
+                    sum = nums[i] + nums[j] + nums[k];
+                    isPrime = isPrime(sum);
+                    if(isPrime) answer++;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public boolean isPrime(int num){
+        for (int t = 2; t <= (int)Math.sqrt(num); t++){
+            if(num%t == 0) return false;
+        }
+        return true;
+    }
+}
