2a50b0f5da26267dfbb7587fd35ec82eae6dc369
TaeSeok <sukyuree@naver.com>
2022-08-15 20:29:05 +0900

    20220815 백준 절댓값 힙 solve

--- test/src/AbsoluteValueHeap.java
+++ test/src/AbsoluteValueHeap.java
@@ -6,41 +6,27 @@ public class AbsoluteValueHeap{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        List<AbsoluteValueAndOwnValue> list = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer obj1, Integer obj2) {
+				if(Math.abs(obj1) > Math.abs(obj2)) {
+					return Math.abs(obj1) - Math.abs(obj2);
+				}else if(Math.abs(obj1) == Math.abs(obj2)) {
+					return obj1 - obj2;
+				}else {
+					return -1;
+				}
+			}
+		});
+
         for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
             int number = Integer.parseInt(st.nextToken());
             if(number == 0){
-                if(list.isEmpty()) System.out.println("0");
-                else{
-                    //뺄 때 정렬하지 말고
-                    Collections.sort(list);
-                    System.out.println(list.get(0).OwnValue);
-                    list.remove(0);
-                }
+                if(pq.isEmpty()) System.out.println("0");
+                else System.out.println(pq.poll());
             }
-            else{
-                //넣을 때 정렬하기?
-                AbsoluteValueAndOwnValue obj = new AbsoluteValueAndOwnValue();
-                obj.OwnValue = number;
-                obj.AbsoluteValue = (int)(obj.OwnValue*obj.OwnValue/Math.sqrt(obj.OwnValue));
-                list.add(obj);
-            }
-        }
-    }
-}
-
-class AbsoluteValueAndOwnValue implements Comparable<AbsoluteValueAndOwnValue>{
-    int AbsoluteValue;
-    int OwnValue;
-
-    @Override
-    public int compareTo(AbsoluteValueAndOwnValue obj) {
-        if(obj.AbsoluteValue < this.AbsoluteValue) return 1;
-        else if (obj.AbsoluteValue > this.AbsoluteValue) return -1;
-        else{
-            if(obj.OwnValue < this.OwnValue) return 1;
-            else return -1;
+            else pq.offer(number);
         }
     }
 }
