480b7ab5b509189f61b7a6ed6df39bb8a199de8c
TaeSeok <sukyuree@naver.com>
2022-08-20 22:27:37 +0900

    20220820 백준 숫자카드 시간초과 해결

--- test/src/NumberCard.java
+++ test/src/NumberCard.java
@@ -2,26 +2,52 @@ import java.util.*;
 import java.io.*;
 
 public class NumberCard {
+    private static int lowerBound(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length;
+
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (target <= arr[mid]) {
+                high = mid;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    private static int upperBound(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length;
+
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (target < arr[mid]) {
+                high = mid;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
 
-        List<Integer> card = new ArrayList<>();
-        List<Integer> numOfCard = new ArrayList<>();
+        int[] cardlist = new int[N];
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++){
-            int input = Integer.parseInt(st.nextToken());
-            if(!card.contains(input)){
-                card.add(input);
-                numOfCard.add(1);
-            }
-            else{
-                int index = card.indexOf(input);
-                numOfCard.set(index,numOfCard.get(index)+1);
-            }
+            cardlist[i] = Integer.parseInt(st.nextToken());
         }
+
+        Arrays.sort(cardlist);
 
         st = new StringTokenizer(br.readLine());
 
@@ -31,12 +57,7 @@ public class NumberCard {
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < M; i++){
             int number = Integer.parseInt(st.nextToken());
-        
-            if(card.contains(number)){
-                int index = card.indexOf(number);
-                sb.append(numOfCard.get(index)).append(" ");
-            }
-            else sb.append(0).append(" ");
+            sb.append(upperBound(cardlist, number) - lowerBound(cardlist, number)).append(" ");
         }
 
         System.out.println(sb);
