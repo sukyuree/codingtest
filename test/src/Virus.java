e819e1e1f15ce1a58350c8aa10ddc666f6b4d655
TaeSeok <sukyuree@naver.com>
2022-08-23 19:00:03 +0900

    20220823 백준 바이러스

---
+++ test/src/Virus.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Virus {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+
+        int node = Integer.parseInt(st.nextToken());
+
+        for(int i = 0; i < node; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+        }
+
+
+    }
+}
