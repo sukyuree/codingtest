ec3d4e388d33c23b82d928a10fc14a288bb1a2a4
TaeSeok <sukyuree@naver.com>
2022-08-27 19:48:23 +0900

    20220827 백준 숨바꼭질

---
+++ test/src/HideAndSeek.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+
+public class HideAndSeek {
+
+    static int N;
+    static int K;
+
+    static int[] visited = new int[100001];
+
+    static boolean[][] graph;
+    static Queue<Integer> q = new LinkedList<>();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+
+        K = Integer.parseInt(st.nextToken());
+
+        int count = bfs();
+
+        System.out.println(count);
+    }
+
+    static int bfs(){
+        visited[N] = 0;
+        q.offer(N);
+        int count = 0;
+        while (q.isEmpty() == false){
+            N = q.poll();
+            
+            if (N == K){
+                count =  visited[N];
+                break;
+            }
+            
+            if (N-1>=0 && visited[N-1] == 0){
+                visited[N-1] = visited[N]+1;
+                q.add(N-1);
+            }
+            if (N+1 <= 100000 && visited[N+1] == 0){
+                visited[N+1] = visited[N]+1;
+                q.add(N+1);
+            }
+            if (2*N <= 100000 && visited[2*N] == 0){
+                visited[2*N] = visited[N] + 1;
+                q.add(2*N);
+            }
+        }
+        return count;
+    }
+    
+}
