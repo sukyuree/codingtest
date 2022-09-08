fb37f6a6585b111ffec4b557be0b1daa8dde4d49
TaeSeok <sukyuree@naver.com>
2022-09-08 20:32:12 +0900

    20220908 백준 바이러스

--- test/src/Virus.java
+++ test/src/Virus.java
@@ -2,11 +2,16 @@ import java.util.*;
 import java.io.*;
 
 public class Virus {
+    static Queue<Integer> q = new LinkedList<>();
+    static boolean[] visited;
+    static boolean[][] graph;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        
         int N = Integer.parseInt(st.nextToken());
+        visited = new boolean[N+1];
+        graph = new boolean[N+1][N+1];
         st = new StringTokenizer(br.readLine());
 
         int node = Integer.parseInt(st.nextToken());
@@ -15,9 +20,24 @@ public class Virus {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
+            graph[a][b] = graph[b][a] = true;
         }
 
-
+        int count = -1;
+        visited[1] = true;
+        q.offer(1);
+        while(!q.isEmpty()) {
+			
+			int V = q.poll();
+            count ++;
+			
+			for(int i = 1 ; i <= N ; i++) {
+				if(graph[V][i] && !visited[i]) {
+					q.offer(i);
+					visited[i] = true;
+				}
+			}
+		}
+        System.out.println(count);
     }
 }
