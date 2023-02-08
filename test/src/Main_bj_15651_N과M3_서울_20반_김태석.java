271f00abc0eeb2d141361df1a12bbe84259d105b
sukyuree <80685310+sukyuree@users.noreply.github.com>
2023-02-08 15:34:38 +0900

    Add files via upload

---
+++ test/src/Main_bj_15651_N과M3_서울_20반_김태석.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+public class Main_bj_15651_N과M3_서울_20반_김태석 {
+    static int N,M;
+    static int[] arr;
+    static int[] pArr;
+    static boolean[] visited;
+        static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        arr = new int[N];
+        pArr = new int[M];
+        visited = new boolean[N];
+
+        for(int i = 1; i <= N; i++){
+            arr[i-1] = i;
+        }
+        permutation(0);
+        System.out.print(sb.toString());
+    }
+
+    static void permutation(int cnt){
+        if(cnt == M){
+            for(int i = 0; i < M; i++){
+                sb.append(pArr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for(int i = 0; i < N; i++){
+            // if(visited[i]) continue;
+            // visited[i] = true;
+            pArr[cnt] = arr[i];
+            permutation(cnt+1);
+            // pArr[cnt] = 0;
+            // visited[i] = false;
+        }
+    }
+
+}
---
+++ test/src/Main_bj_15656_N과M8_서울_20반_김태석.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main_bj_15656_N과M8_서울_20반_김태석 {
+    static int N,M;
+    static int[] arr;
+    static int[] pArr;
+    static boolean[] visited;
+        static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        arr = new int[N];
+        pArr = new int[M];
+        visited = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 1; i <= N; i++){
+            arr[i-1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        combination(0,0);
+        System.out.print(sb.toString());
+    }
+    
+    static void combination(int cnt, int start){
+        if(cnt == M) {
+            for(int i = 0; i < M; i++){
+                sb.append(pArr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = start; i < N; i++){
+            pArr[cnt] = arr[i];
+            combination(cnt+1,i);
+        }
+    }
+}
