f13d52ba2d80340871dc87c53da9103090d3fb2e
sukyuree <80685310+sukyuree@users.noreply.github.com>
2023-02-15 14:07:37 +0900

    Update Main_bj_11286_절댓값힙.java

--- test/src/Main_bj_11286_절댓값힙.java
+++ test/src/Main_bj_11286_절댓값힙.java
@@ -6,27 +6,23 @@ public class Main_bj_11286_절댓값힙{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
-			@Override
-			public int compare(Integer obj1, Integer obj2) {
-				if(Math.abs(obj1) > Math.abs(obj2)) {
-					return Math.abs(obj1) - Math.abs(obj2);
-				}else if(Math.abs(obj1) == Math.abs(obj2)) {
-					return obj1 - obj2;
-				}else {
-					return -1;
-				}
-			}
-		});
+        PriorityQueue<Integer> pq = new PriorityQueue<>((p1,p2)->{
+            if(Math.abs(p1) == Math.abs(p2)) return p1 - p2;
+            return Math.abs(p1)-Math.abs(p2);
+        });
+        
+        StringBuilder sb = new StringBuilder();
 
         for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
             int number = Integer.parseInt(st.nextToken());
             if(number == 0){
-                if(pq.isEmpty()) System.out.println("0");
-                else System.out.println(pq.poll());
+                if(pq.isEmpty()) sb.append(0).append("\n");
+                else sb.append(pq.poll()).append("\n");
             }
             else pq.offer(number);
         }
+
+        System.out.print(sb.toString());
     }
 }
