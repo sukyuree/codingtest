67a0c405eaf39a2ec0a83040366ad22eaccda2e2
sukyuree <80685310+sukyuree@users.noreply.github.com>
2023-02-16 17:55:13 +0900

    Add files via upload

---
+++ test/src/Solution_4012_요리사_서울_20반_김태석.java
@@ -0,0 +1,72 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution_4012_요리사_서울_20반_김태석 {
+
+    static int N;
+    static int[][] arr;
+    static boolean[] visited;
+    static int min = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        
+        for(int test_case =1 ; test_case<=T;test_case++) {
+            
+            N = Integer.parseInt(br.readLine());
+            arr = new int[N][N];
+            visited = new boolean[N];
+
+            for(int i=0;i<N;i++) {
+                st = new StringTokenizer(br.readLine());
+                for(int j=0;j<N;j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            min = Integer.MAX_VALUE;
+  
+            comb(0,0);
+
+            sb.append("#").append(test_case).append(" ").append(min).append("\n");
+        }
+        
+        System.out.println(sb);
+        
+        
+        br.close();
+    }
+
+    static void cal() {
+        int sumA =0;
+        int sumB =0;
+        
+        for(int i=0;i<N;i++) {
+            for(int j=i+1;j<N;j++) {            
+                if(visited[i] && visited[j]) {
+                    sumA+= (arr[i][j] + arr[j][i]);
+                }else if(!visited[i] && !visited[j]) {
+                    sumB+=(arr[i][j] + arr[j][i]);
+                }
+                
+            }
+        }
+        
+        min = Math.min(Math.abs(sumA-sumB), min);
+    }
+    
+    static void comb(int cnt, int start) {
+        if(cnt==N/2) {
+            cal();
+            return;
+        }
+        for(int i=start;i<N;i++) {
+            visited[i] = true;
+            comb(cnt+1, i+1);
+            visited[i] = false;
+        }
+    }
+}
