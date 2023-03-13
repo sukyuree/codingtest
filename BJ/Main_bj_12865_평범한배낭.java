6e9e32351bdb77d2ef561692b84c99ecf9789714
sukyuree <80685310+sukyuree@users.noreply.github.com>
2023-03-13 16:35:58 +0900

    백준_평범한배낭
    DP
    배낭

--- BJ/Main_bj_12865_평범한배낭.java
+++ BJ/Main_bj_12865_평범한배낭.java
@@ -2,87 +2,34 @@ import java.util.*;
 import java.io.*;
 
 public class Main_bj_12865_평범한배낭 {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int[] dp = new int[100001];
-        boolean[] dpVisited = new boolean[100001];
-
-        PriorityQueue<Integer>[] list = new PriorityQueue[100001];
-        for(int i = 0; i <= 100000; i++){
-            list[i] = new PriorityQueue<>((p1,p2)->p2-p1);
+        int[] W = new int[N + 1];
+        int[] V = new int[N + 1];
+        int[][] dp = new int[N + 1][K + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            W[i] = Integer.parseInt(st.nextToken());
+            V[i] = Integer.parseInt(st.nextToken());
         }
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= K; j++) {
 
-        
-        for(int i = 0 ; i < N; i++){
-            st = new StringTokenizer(br.readLine());
-            int index = Integer.parseInt(st.nextToken());
-            int value = Integer.parseInt(st.nextToken());
-            list[index].offer(value);
-        }
+                // i번째 무게를 더 담을 수 없는 경우
+                if (W[i] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                }
+                // i번째 무게를 더 담을 수 있는 경우
+                else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - W[i]] + V[i]);
+                }
 
-        dp[0] = 0;
-
-        for(int i = 1; i <= K; i++){
-            int max = 0;
-            int finalIndex = 0;
-            int J = 0;
-            int finalJ = 0;
-            int index = 0;
-            int temp1 = 0;
-            int temp2 = 0;
-            for(int j = 1; j <= i; j++){
-                if(dp[j]!=0&&dp[i-j]!=0&&!dpVisited[j]&&!dpVisited[i-j]&&j!=i-j){
-                    temp1 = dp[j]+dp[i-j];
-                    J = j;
-                }
-                if(!list[j].isEmpty()){
-                    if(j == i){
-                        temp2 = list[j].peek();
-                        index = j;
-                    }
-                    else if(j<i&&dp[i-j]!=0&&!dpVisited[i-j]){
-                        temp2 = list[j].peek() + dp[i-j];
-                        index = j;
-                    }
-                }
-                max = Math.max(max,Math.max(temp1,temp2));
-
-                if(max == temp1){
-                    finalIndex = 0;
-                    finalJ = J;
-                }
-                else if(max == temp2){
-                    finalIndex = index;
-                    finalJ = 0;
-                }
-                
-                if(temp1 == temp2){
-                    finalIndex = 0;
-                    finalJ = J;
-                }
-            }
-
-            if(finalIndex == 0 && finalJ == 0) dp[i] = 0;
-            else if(finalIndex == 0 && finalJ != 0){
-                dp[i] = dp[finalJ] + dp[i - finalJ];
-                dpVisited[finalJ] = true;
-                dpVisited[i - finalJ] = true;
-            }
-            else if(finalIndex != 0 && finalJ == 0){
-                dp[i] = list[finalIndex].poll() + dp[i - finalIndex];
-                dpVisited[i-finalIndex] = true;
             }
         }
-
-        for(int i = K; i >= 1; i--){
-            if(dp[i]!=0){
-                System.out.print(dp[i]);
-                break;
-            }
-        }
+        System.out.println(dp[N][K]);
     }
 }
