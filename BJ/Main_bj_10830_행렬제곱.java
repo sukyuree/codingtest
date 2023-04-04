497ab80df7d4f10025405acfc98cafe49502d786
sukyuree <80685310+sukyuree@users.noreply.github.com>
2023-04-04 15:26:47 +0900

    백준_행렬제곱_행렬제곱하는 방법을 알아야함, 분할정복을 했던 제곱과 비슷한 문제.

---
+++ BJ/Main_bj_10830_행렬제곱.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_bj_10830_행렬제곱 {
+  static final int MOD = 1000;
+  static int N;
+  static long B;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    StringBuilder sb = new StringBuilder();
+    N = Integer.parseInt(st.nextToken());
+    B = Long.parseLong(st.nextToken());
+
+    int[][] A = new int[N][N];
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < N; j++) {
+        A[i][j] = Integer.parseInt(st.nextToken());
+      }
+    }
+    int[][] result = pow(A,B);
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        sb.append(result[i][j] + " ");
+      }
+      sb.append("\n");
+    }
+    System.out.print(sb.toString());
+  }
+
+  public static int[][] pow(int[][] A, long count) {
+ 
+		if(count == 1L) {
+			return A;
+		}
+		
+		int[][] ret = pow(A, count / 2);
+		
+		ret = multiply(ret, ret);
+		
+		if(count % 2 == 1L) {
+			ret = multiply(ret, A);
+		}
+		
+		return ret;
+	}
+
+  static int[][] multiply(int[][] arr1, int[][] arr2) {
+		
+		int[][] ret = new int[N][N];
+		
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				for(int k = 0; k < N; k++) {
+					
+					ret[i][j] += arr1[i][k] * arr2[k][j];
+					ret[i][j] %= MOD;	
+				}
+			}
+		}
+		return ret;
+	}
+}
